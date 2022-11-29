2ee27d4e361ab43b5333be56a8c35cedd533d616
Jorge Enrique Leporace <67753773+jorgeleporace@users.noreply.github.com>
2022-11-29 11:52:14 -0300

    agreguePaginaDePago

--- sections/compra.js
+++ sections/compra.js
@@ -7,3 +7,16 @@ function agregarCarrito(nombreCurso,precio) {
     document.getElementById("totalAbonar").innerHTML="Total a Abonar = $"+total;
     localStorage.setItem('carrito',JSON.stringify(carrito));
 }
+
+function pagar(){
+    window.location.href="http://127.0.0.1:5500/sections/abonar.html";
+}
+function inicializar(){
+    let carrito = JSON.parse(localStorage.getItem('carrito'))
+    this.total = 0;
+    carrito.forEach(element => {
+        this.total += element.precio;
+
+    });
+    document.getElementById("totalAbonar").innerHTML="Total a Abonar = $"+this.total;
+}
